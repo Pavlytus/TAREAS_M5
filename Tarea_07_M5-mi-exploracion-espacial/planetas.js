d958c590f8e008e185321a06831996a5855dc287
Pavlytus <pavlo.6cn@gmail.com>
2025-06-26 20:48:25 -0600

    trajando con nodejs y npm

---
+++ Tarea_07_M5-mi-exploracion-espacial/planetas.js
@@ -0,0 +1,39 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/0/07/Titan_in_true_color.jpg",
+    distanciaALaTierra: 1.2 // en miles de millones de km (~0.001 año luz)
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Exoplanet_Comparison_Proxima_b.png",
+    distanciaALaTierra: 4.24
+  },
+  {
+    nombre: "Kepler-452b",
+    descripcion: "Un planeta potencialmente habitable, considerado una 'Tierra 2.0'.",
+    descubiertoEn: "2015",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/5/52/Kepler-452b_Artist_Concept.jpg",
+    distanciaALaTierra: 1400
+  },
+  {
+    nombre: "Gliese 581g",
+    descripcion: "Un exoplaneta potencialmente habitable en la zona habitable de su estrella.",
+    descubiertoEn: "2010",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Gliese_581_g_art.png",
+    distanciaALaTierra: 20.3
+  },
+  {
+    nombre: "Trappist-1e",
+    descripcion: "Uno de los siete planetas del sistema TRAPPIST-1, con posibilidad de agua líquida.",
+    descubiertoEn: "2017",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/3/3f/PIA21423_-_TRAPPIST-1e.jpg",
+    distanciaALaTierra: 39.5
+  }
+];
+
+module.exports = planetas;
